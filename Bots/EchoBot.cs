c6381c5f2066b5bb0f68906f213ac38e49d36a64
Tomasz_Trzcinski <Tomasz_Trzcinski>
2019-08-27 17:42:06 +0200

    Changed welcome message.

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -22,7 +22,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome!"), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello and welcome, user!"), cancellationToken);
                 }
             }
         }
